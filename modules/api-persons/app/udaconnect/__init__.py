be235cb89da1603be3a96274ec85027e697e7cb1
Joe Chien <cchla1021@yahoo.com>
2022-05-15 23:15:12 +0000

    correct persons and connection microservices route

--- modules/api-persons/app/udaconnect/__init__.py
+++ modules/api-persons/app/udaconnect/__init__.py
@@ -1,8 +1,8 @@
-from app.udaconnect.models import Connection, Location, Person  # noqa
-from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema  # noqa
+from app.udaconnect.models import Person  # noqa
+from app.udaconnect.schemas import PersonSchema  # noqa
 
 
-def register_routes(api, app, root="api"):
+def register_routes(api, app, root="api-persons"):
     from app.udaconnect.controllers import api as udaconnect_api
 
     api.add_namespace(udaconnect_api, path=f"/{root}")
